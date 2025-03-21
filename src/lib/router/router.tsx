723852fce9ab11ae5f50597b3b6b0f0003c35fae
myjeong19 <myjeong19@naver.com>
2025-03-21 12:26:58 +0900

    🔧 리액트쿼리 설정

---
+++ src/lib/router/router.tsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+const browserRouter = createBrowserRouter([]);
+
+export const Router = () => <RouterProvider router={browserRouter} />;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Router } from './lib/router';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/query';
 
-createRoot(document.getElementById('root')!).render(<StrictMode>{/* <App /> */}</StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  </StrictMode>
+);

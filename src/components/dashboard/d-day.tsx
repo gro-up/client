43938bd1f0b3b7bdcc76d1f650dedb22b79a49cb
myjeong19 <myjeong19@naver.com>
2025-04-01 21:09:29 +0900

    fix. dday nan 오류

--- src/components/dashboard/d-day.tsx
+++ src/components/dashboard/d-day.tsx
@@ -1,33 +1,41 @@
-import { Hourglass } from 'lucide-react';
+import { Hourglass, Plus } from 'lucide-react';
 import { differenceInDays } from 'date-fns';
 import { ShadowContainer } from '../ui';
 import { mockData } from '../schedule/schedule.table';
 
-import { Badge } from '../shadcn-ui';
+import { Badge, Button } from '../shadcn-ui';
 
 export const DDay = () => {
   const today = new Date();
 
   const filteredDate = mockData.filter(item => {
-    const itemDate = new Date(item.time);
+    const itemDate = new Date(item?.time);
     return itemDate > today;
   });
 
-  const dDay = differenceInDays(filteredDate[0].time, today);
+  const dDay = differenceInDays(filteredDate[0]?.time, today);
 
   return (
-    <ShadowContainer className="flex flex-col gap-2 w-80 min-w-[350px]  min-h-[50px]">
-      <div className="flex gap-2 w-full justify-between items-center ">
-        <Badge className="p-4 h-full bg-blue-400 rounded-br-none rounded-tr-full ">
-          <Hourglass className="w-5 h-5 ml-4 fill-white" />
-          <p>D-{dDay}</p>
-        </Badge>
-        <div className="flex gap-2 p-4">
-          <p className="text-md">{filteredDate[0]?.company}</p>
-          <p className="text-md">{filteredDate[0]?.status}</p>
-          <p>{filteredDate[0]?.location}</p>
+    <>
+      <ShadowContainer className="flex flex-col gap-2 w-80 min-w-[350px]  min-h-[50px]">
+        <div className="flex gap-2 w-full justify-between items-center ">
+          <Badge className="p-4 h-full bg-blue-400 rounded-br-none rounded-tr-full ">
+            <Hourglass className="w-5 h-5 ml-4 fill-white" />
+            <p>D-{filteredDate.length > 0 ? dDay : 0}</p>
+          </Badge>
+          {filteredDate.length > 0 ? (
+            <div className="flex gap-2 p-4">
+              <p className="text-md">{filteredDate[0]?.company}</p>
+              <p className="text-md">{filteredDate[0]?.status}</p>
+              <p>{filteredDate[0]?.location}</p>
+            </div>
+          ) : (
+            <Button variant="outline" className="flex justify-center items-center">
+              <Plus className="w-5 h-5 ml-4 fill-white" />
+            </Button>
+          )}
         </div>
-      </div>
-    </ShadowContainer>
+      </ShadowContainer>
+    </>
   );
 };

d028f16f3fe39c810fb2df1d62e22a3814cea346
myjeong19 <myjeong19@naver.com>
2025-04-05 10:44:27 +0900

    style. 오늘 페이지 캘린더 추가

--- src/components/ui/date-time-picker.tsx
+++ src/components/ui/date-time-picker.tsx
@@ -6,9 +6,10 @@ import { format } from 'date-fns';
 interface DateTimePickerProps {
   date: Date;
   onDate: (day: Date) => void;
+  type?: 'viewer' | 'editor';
 }
 
-export const DateTimePicker = ({ date, onDate }: DateTimePickerProps) => {
+export const DateTimePicker = ({ date, onDate, type = 'viewer' }: DateTimePickerProps) => {
   return (
     <>
       <DayPicker
@@ -18,12 +19,12 @@ export const DateTimePicker = ({ date, onDate }: DateTimePickerProps) => {
         classNames={{
           month: 'w-full',
           months: 'w-full',
-          caption: 'relative h-10 flex items-center justify-center',
+          caption: 'relative h-10 flex items-center justify-start pl-5',
           nav_button_next: 'absolute bg-transparent top-1/2 -translate-y-1/2 right-5 text-gray-400',
           nav_button_previous:
-            'absolute bg-transparent top-1/2 -translate-y-1/2 left-5 text-gray-400',
+            'absolute bg-transparent top-1/2 -translate-y-1/2 right-15 text-gray-400',
           head: 'w-full text-center',
-          cell: 'w-10 text-center',
+          cell: 'w-15 text-center',
           day_selected: 'text-blue-500',
           day_today: 'bg-blue-100 rounded-full',
           day: 'p-3',
@@ -34,16 +35,18 @@ export const DateTimePicker = ({ date, onDate }: DateTimePickerProps) => {
           }
         }}
       />
-      <Input
-        type="time"
-        value={format(date, 'HH:mm')}
-        onChange={e => {
-          const time = new Date(date.getTime());
-          time.setHours(Number(e.target.value.split(':')[0]));
-          time.setMinutes(Number(e.target.value.split(':')[1]));
-          onDate(time);
-        }}
-      />
+      {type === 'editor' && (
+        <Input
+          type="time"
+          value={format(date, 'HH:mm')}
+          onChange={e => {
+            const time = new Date(date.getTime());
+            time.setHours(Number(e.target.value.split(':')[0]));
+            time.setMinutes(Number(e.target.value.split(':')[1]));
+            onDate(time);
+          }}
+        />
+      )}
     </>
   );
 };
--- src/pages/today-page.tsx
+++ src/pages/today-page.tsx
@@ -4,11 +4,12 @@ import {
   ScheduleDetailContent,
   ScheduleDetailHeader,
 } from '@/components/schedule';
+import { DateTimePicker } from '@/components/ui';
 
 export default function TodayPage() {
   return (
     <>
-      <div className="flex flex-col gap-2 min-w-[350px] w-full  h-full">
+      <div className="flex flex-col gap-2 min-w-[350px] w-full  h-full max-h-[850px]">
         <ScheduleHeader />
         <main className="bg-gray-50 rounded-md p-3 min-h-[776px] h-full">
           <ScheduleDetailHeader />
@@ -16,7 +17,9 @@ export default function TodayPage() {
         </main>
       </div>
 
-      <div className="bg-gray-50 min-w-[350px] max-w-[400px] w-full rounded-md h-full p-3">
+      <div className="bg-gray-50 min-w-[350px] max-w-[400px] max-h-[850px] w-full rounded-md h-full p-3 overflow-hidden">
+        <DateTimePicker date={new Date()} onDate={() => {}} type="viewer" />
+        <hr />
         <ScheduleList />
       </div>
     </>

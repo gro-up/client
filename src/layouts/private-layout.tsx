b891fa836f1163112d591de85dbb607b7de10e55
myjeong19 <myjeong19@naver.com>
2025-04-09 19:35:24 +0900

    feat. 테마 제공을 위한 컨텍스트 및 훅 추가

--- src/layouts/private-layout.tsx
+++ src/layouts/private-layout.tsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from 'react-router';
 import { ON_STEP_TOKEN_NAME, useCookies } from '@/hooks/auth';
 import { ROUTER_PATH } from '@/router';
 import { Sidebar } from '@/components/sidebar';
+import { ThemeProvider } from '@/context/theme';
 
 export const PrivateLayout = () => {
   const { cookies } = useCookies();
@@ -11,12 +12,14 @@ export const PrivateLayout = () => {
   }
 
   return (
-    <main className="flex justify-center items-center gap-4 bg-gray-200 w-full h-screen px-4 ">
-      <Sidebar />
+    <ThemeProvider defaultTheme="dark" storageKey="gro-ui-theme">
+      <main className="flex justify-center items-center gap-4 w-full h-screen px-4 ">
+        <Sidebar />
 
-      <div className="flex justify-center items-center w-full min-h-[750px] h-11/12 rounded-md gap-2 relative">
-        <Outlet />
-      </div>
-    </main>
+        <div className="flex justify-center items-center w-full min-h-[750px] h-11/12 rounded-md gap-2 relative">
+          <Outlet />
+        </div>
+      </main>
+    </ThemeProvider>
   );
 };

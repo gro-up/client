b9f64dbeae2d4e3184655bc5a5ab799a6110c803
lkj1313 <smm45108@gmail.com>
2025-04-27 16:06:18 +0900

    fix : 로그인페이지에서 회원가입 페이지로 페이지  전환 안되던 부분 수정

--- src/components/shadcn/button.tsx
+++ src/components/shadcn/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         mint: "bg-emerald-400 text-white shadow-xs hover:bg-emerald-600 cursor-pointer", // 기본 민트색(emerald-400), hover 시 emerald-500으로 진하게
         google:
-          "bg-white text-black shadow-xs cursor-pointer hover:bg-gray-200", // 구글 버튼: 흰 배경 + 검정 글자, hover 시 회색톤으로 자연스럽게
+          "bg-white text-black shadow-xs cursor-pointer hover:bg-gray-200", //  흰 배경 + 검정 글자, hover 시 회색톤으로 자연스럽게
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { CookiesProvider } from "react-cookie";
-import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import { Router, ROUTER_PATH } from "./router";
 import { queryClient } from "./query";
@@ -12,10 +12,8 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <CookiesProvider defaultSetOptions={{ path: ROUTER_PATH.PUBLIC.LANDING }}>
-        <BrowserRouter>
-          <Router />
-          <Toaster />
-        </BrowserRouter>
+        <Router />
+        <Toaster />
       </CookiesProvider>
     </QueryClientProvider>
   </StrictMode>

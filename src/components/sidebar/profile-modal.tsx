ed4e6c637482b32e703b829136b0f7b3aa7f27bf
lkj1313 <smm45108@gmail.com>
2025-06-02 19:44:03 +0900

    fix: 프로필모달 ui 변경

--- src/components/sidebar/profile-modal.tsx
+++ src/components/sidebar/profile-modal.tsx
@@ -10,6 +10,7 @@ import { useProfile } from "@/hooks/auth";
 import { useRef } from "react";
 import { useImagePreview } from "@/hooks/auth/use-imge-preview";
 import { useUploadProfileImage } from "@/hooks/auth/use-upload-profile-image";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
 interface Props {
   isOpen: boolean;
@@ -27,12 +28,15 @@ export default function ProfileModal({ isOpen, onClose }: Props) {
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="flex flex-col gap-2.5">
         <DialogHeader>
-          <DialogTitle className="mx-[35px] p-2.5">
-            <div className="px-[5px] py-2.5">프로필</div>
+          <DialogTitle className="mx-[35px] pb-2.5">
+            <div className=" ">프로필</div>{" "}
           </DialogTitle>
+          <DialogDescription className="text-sm text-muted-foreground mx-[35px] ">
+            프로필 사진을 업로드하거나 변경할 수 있습니다.
+          </DialogDescription>
         </DialogHeader>
 
-        <div className="h-[340px] p-2.5 w-full flex flex-col items-center gap-2.5">
+        <div className="h-[340px] p-2.5 w-full flex flex-col items-center justify-center gap-2.5">
           <div className="flex flex-col gap-2.5">
             <div className="w-50 h-50 bg-white rounded-[20px] relative overflow-hidden">
               {previewUrl ? (
@@ -57,7 +61,7 @@ export default function ProfileModal({ isOpen, onClose }: Props) {
                 +
               </Button>
             </div>
-            <div className="flex w-50 h-[50px] items-center justify-center text-sm text-gray-600">
+            <div className="flex w-50 h-[50px] items-center justify-center text-sm text-white">
               {profile.email}
             </div>
           </div>
@@ -67,7 +71,7 @@ export default function ProfileModal({ isOpen, onClose }: Props) {
           <Button
             onClick={onClose}
             size="lg"
-            className="w-[100px] h-[35px] bg-neutral-900 hover:bg-neutral-700 text-white"
+            className="w-[100px] h-[35px] bg-neutral-900 hover:bg-neutral-700 text-white cursor-pointer"
             type="button"
             disabled={isLoading}
           >
